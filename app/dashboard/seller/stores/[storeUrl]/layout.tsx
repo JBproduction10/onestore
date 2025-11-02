a6b70cdd002ec4db46da2b5406fa8448c87655d4
Jonathan Bangala <jbangala90@gmail.com>
2025-11-02 07:50:06 +0200

    working on core functionality

---
+++ app/dashboard/seller/stores/[storeUrl]/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+export default async function SellerUrlLayout({
+    children,
+}: {
+    children: ReactNode;
+}) {
+    
+    return <div>{children}</div>
+}
